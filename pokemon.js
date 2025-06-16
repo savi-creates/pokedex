050773fe305dbd51c4027578f53248948f45776f
savi-creates <savannahandresson@gmail.com>
2025-06-16 10:52:30 +0100

    Created JS to fetch from API

---
+++ pokemon.js
@@ -0,0 +1,16 @@
+const MAX_POKEMON = 151;
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
+
+let allPokemons = [];
+
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
+  .then((response) => response.json())
+  .then((data) => {
+    allPokemons = data.results;
+  });
+
+async function fetchPokemonDataBeforeRedirect(id) {}
